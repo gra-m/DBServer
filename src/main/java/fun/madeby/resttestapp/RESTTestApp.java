65b3f45af713333253a32cd974e2fd680fdb09ef
gra-m <91182298+gra-m@users.noreply.github.com>
2022-07-21 17:37:07 +0200

    #38 extra threads and sendRequest() added

--- src/main/java/fun/madeby/resttestapp/RESTTestApp.java
+++ src/main/java/fun/madeby/resttestapp/RESTTestApp.java
@@ -10,7 +10,10 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.logging.Logger;
+
+import static java.lang.Thread.currentThread;
 
 /**
  * Created by Gra_m on 2022 07 21
@@ -27,7 +30,7 @@ public class RESTTestApp {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		RESTTestApp app = new RESTTestApp();
 		while(true) {
 			app.performTest();
@@ -39,13 +42,56 @@ public class RESTTestApp {
 		}
 	}
 
-	private void performTest() {
-		Map<String, String> parameters = new HashMap<>();
-		parameters.put("name", "test");
+	private void performTest() throws InterruptedException {
+		
+		Thread searchTest = new Thread(() -> {
+		while(true)	 {
+			performSearchTest();
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		});
+		
+		Thread returnAllRecordsTest = new Thread(() -> {
+			while(true) {
+				performReturnAllRecordsTest();
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		Thread addRecordTest = new Thread(() -> {
+			while(true) {
+				performAddRecordTest();
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		//searchTest.start();
+		//returnAllRecordsTest.start();
+		addRecordTest.start();
+
+	}
+
+	private void sendRequest(String path, Map<String, String> parameters) {
+		StringBuilder sb = new StringBuilder();
+			sb.append("http://localhost:7001/").append(path);
+
+		if (parameters != null)
+			sb.append("?").append(new BuiltParameter().getParametersAsString(parameters));
 
 		try{
-			//send request
-			URL url = new URL("http://localhost:7001/searchlevenshtein?" + new BuiltParameter().getParametersAsString(parameters));
+			URL url = new URL(sb.toString());
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("GET");
 			con.setConnectTimeout(1000);
@@ -53,7 +99,7 @@ public class RESTTestApp {
 
 			//read & log response
 			int responseCode = con.getResponseCode();
-			LOGGER.info("con.getResponseCode() =" + responseCode);
+			LOGGER.info("con.getResponseCode() on currentThread.getName() =  " + responseCode +  " " + currentThread().getName());
 			if (responseCode == 200){
 				String responseResult = readResponseStream(con.getInputStream());
 				LOGGER.info("readResponseStream(con.getInputStream() returned: \n" + responseResult);
@@ -64,9 +110,36 @@ public class RESTTestApp {
 		}catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-			LOGGER.severe("@performTest url.openConnection()");
+			LOGGER.severe("@sendRequest url.openConnection()");
 			e.printStackTrace();
 		}
+
+	}
+	private void performAddRecordTest() {
+		Map<String, String> parameters = new HashMap<>();
+		int i = new Random().nextInt(0, 100);
+		int x = new Random().nextInt(0, 4000);
+		parameters.put("name", "test" + i + "_" + x);
+		parameters.put("age", "345");
+		parameters.put("address", "1234, Acacia av");
+		parameters.put("carplate", "PYP-729Y");
+		parameters.put("description", "This is a very interesting description describing how this description was written and why I ..");
+
+		LOGGER.info("@performAddRecordTest about to add ->" + parameters.get("name"));
+		sendRequest("add", parameters);
+	}
+
+	private void performReturnAllRecordsTest() {
+		sendRequest("listall", null);
+
+	}
+
+	private void performSearchTest() {
+		Map<String, String> parameters = new HashMap<>();
+		parameters.put("name", "test");
+
+		sendRequest("searchlevenshtein", parameters);
+
 	}
 
 	private String readResponseStream(InputStream inputStream) {
